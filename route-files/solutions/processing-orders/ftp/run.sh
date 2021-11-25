c330e68342f528b91a2b8ac3bc4d27b827e58fe6
Jaime Ramirez <jaime.ramirez@redhat.com>
2021-11-25 13:45:18 +0100

    Add wrapper scripts to run the FTP server

--- route-files/solutions/processing-orders/ftp/run.sh
+++ route-files/solutions/processing-orders/ftp/run.sh
@@ -3,5 +3,15 @@
 # Make sure the current working dir is ftp/
 cd "$(dirname "$0")"
 
+printf "\033[1;32mBuild FTP server image...\033[0m\n"
+
 podman build -f Containerfile -t ad221-vsftpd container
-podman run  --rm -ti -p 21720:20 -p 21721:21 -p 21100-21110:21100-21110  -v $(pwd)/data:/home/datauser --name ad221-ftp-server ad221-vsftpd
+
+printf "\n\n\033[1;32mStart FTP server...\033[0m\n"
+
+podman run --rm -ti \
+    --name ad221-ftp-server \
+    --user "$(id -u):$(id -g)" \
+    -p 21720:20 -p 21721:21 -p 21100-21110:21100-21110 \
+    -v $(pwd)/data:/home/datauser \
+    ad221-vsftpd
