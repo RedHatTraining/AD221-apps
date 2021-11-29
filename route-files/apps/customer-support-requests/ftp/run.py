ff7e354c92a9c4c53c3974f01ae09e4f4b94a973
Jaime Ramirez <jaime.ramirez@redhat.com>
2021-11-29 10:21:58 +0100

    add apps

---
+++ route-files/apps/customer-support-requests/ftp/run.py
@@ -0,0 +1,22 @@
+import subprocess
+from pathlib import Path
+
+cwd = Path(__file__).parent.resolve()
+
+print("\033[1;32mBuild FTP server image\033[0m\n")
+subprocess.Popen(
+    "podman build -f Containerfile -t ad221-vsftpd container",
+    cwd=cwd,
+    shell=True
+).communicate()
+
+print("\n\n\033[1;32mStart FTP server\033[0m\n")
+subprocess.Popen(
+    "podman run --rm -ti"
+    "  --name ad221-ftp-server"
+    "  -p 21720:20 -p 21721:21 -p 21100-21110:21100-21110"
+    "  -v $(pwd)/data:/home/datauser"
+    "  ad221-vsftpd",
+    cwd=cwd,
+    shell=True
+).communicate()
