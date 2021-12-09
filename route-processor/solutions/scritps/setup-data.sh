9f279b01e242e607ce8349bb862dae750d4912f3
Eduardo Ramirez <eramirez@redhat.com>
2021-12-09 12:22:13 +0100

    Remove unnecessary script

--- route-processor/solutions/scritps/setup-data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo 'Preparing test folder:'
-echo '  Cleaning test folder...'
-rm -rf orders
-mkdir -p orders/incoming
-echo '  Copying sample data files...'
-cp ../../resources/data/orders/* orders/incoming
-echo 'Preparation complete!'
