fff77381448dc03a39aec063a946191456341577
Eduardo Ramirez <eramirez@redhat.com>
2021-12-09 12:50:17 +0100

    Add imports to Route class

--- route-processor/apps/orders-processor/src/main/java/com/redhat/training/processor/FileRouteBuilder.java
+++ route-processor/apps/orders-processor/src/main/java/com/redhat/training/processor/FileRouteBuilder.java
@@ -1,5 +1,11 @@
 package com.redhat.training.processor;
 
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
