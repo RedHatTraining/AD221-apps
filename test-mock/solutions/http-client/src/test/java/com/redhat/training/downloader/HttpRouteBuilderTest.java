795978034edd30d8a16114ea7b5807267d6a404b
Jaime Ramírez <jaime.ramirez@redhat.com>
2022-01-11 13:29:00 +0100

    Apply suggestions from code review
    Co-authored-by: Eduardo Ramirez <42619854+ebramirez@users.noreply.github.com>

--- test-mock/solutions/http-client/src/test/java/com/redhat/training/downloader/HttpRouteBuilderTest.java
+++ test-mock/solutions/http-client/src/test/java/com/redhat/training/downloader/HttpRouteBuilderTest.java
@@ -12,7 +12,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @RunWith( CamelSpringBootRunner.class )
 @SpringBootTest(properties = {
-	//TODO: add properties
 	"http_route.start=direct:start",
     "http_route.server=http4://test-fake"
 })
@@ -24,28 +23,23 @@ public class HttpRouteBuilderTest {
 	@Autowired
 	private ProducerTemplate template;
 
-	// TODO: add @EndpointInject annotation
 	@EndpointInject(uri = "mock:http4:test-fake/greeting")
 	MockEndpoint httpMockEndpoint;
 
-	// TODO: add @EndpointInject annotation
 	@EndpointInject(uri = "mock:file:out")
 	MockEndpoint fileMockEndpoint;
 
 	@Test
 	public void testFileRecievesContentFromHttpClient() throws InterruptedException {
-		// TODO: add httpMockEndpoint behaviour
 		httpMockEndpoint.whenAnyExchangeReceived(e -> {
 			e.getOut().setBody("Hello test!");
 		});
 
-		// TODO: add fileMockEndpoint expectations
 		fileMockEndpoint.expectedMessageCount(1);
 		fileMockEndpoint.expectedBodiesReceived("Hello test!");
 
 		template.sendBody("direct:start", null);
 
-		// TODO: assert fileMockEndpoint
 		fileMockEndpoint.assertIsSatisfied();
 	}
 
