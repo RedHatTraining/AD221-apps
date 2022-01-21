949d2e021251ff725022683d9b0b12009c105de2
Jaime Ramirez <jaime.ramirez@redhat.com>
2022-01-21 18:37:00 +0100

    fixes

--- cloud-deploy/solutions/temperatures-route/src/main/java/com/redhat/training/TemperaturesRESTRouteBuilder.java
+++ cloud-deploy/solutions/temperatures-route/src/main/java/com/redhat/training/TemperaturesRESTRouteBuilder.java
@@ -27,7 +27,7 @@ public class TemperaturesRESTRouteBuilder extends RouteBuilder {
                     .getIn()
                     .setHeader("error", exchange.getProperty(Exchange.EXCEPTION_CAUGHT, Exception.class));
             })
-            // TODO: use the route-health bean to set health status to down
+            // TODO: use the route-health bean to set health down
             .bean("route-health", "down");
 
         rest("/")
@@ -45,7 +45,7 @@ public class TemperaturesRESTRouteBuilder extends RouteBuilder {
             .unmarshal()
             .json(JsonLibrary.Jackson)
             .bean(TemperaturesConverter.class, "valuesToFahrenheit")
-            // TODO: use the route-health bean to set health status to up
+            // TODO: use the route-health bean to set health up
             .wireTap("bean:route-health?method=up");
     }
 }
