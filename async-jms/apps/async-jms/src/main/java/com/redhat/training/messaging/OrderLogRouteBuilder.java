f30571e3f7ac71178a3143f8638bcb170c0bc05c
Randy Thomas <althomas@redhat.com>
2022-01-28 09:18:46 -0600

    updated to include the details for this route in apps

--- async-jms/apps/async-jms/src/main/java/com/redhat/training/messaging/OrderLogRouteBuilder.java
+++ async-jms/apps/async-jms/src/main/java/com/redhat/training/messaging/OrderLogRouteBuilder.java
@@ -8,9 +8,10 @@ public class OrderLogRouteBuilder extends RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
-
-		// TODO receive messages from Log_Orders queue and send to the mock fulfillment end point
-
-		}
+		from("jms:queue:Log_Orders")
+			.routeId("Log Orders")
+			.log("Order received: ${body}")
+			.to("mock:fulfillmentSystem");
+	}
 
 }
--- async-jms/solutions/async-jms/src/main/java/com/redhat/training/messaging/OrderLogRouteBuilder.java
+++ async-jms/solutions/async-jms/src/main/java/com/redhat/training/messaging/OrderLogRouteBuilder.java
@@ -9,7 +9,6 @@ public class OrderLogRouteBuilder extends RouteBuilder {
 	@Override
 	public void configure() throws Exception {
 
-		// TODO receive messages from Log_Orders queue and send to the mock fulfillment end point
 		from("jms:queue:Log_Orders")
 			.routeId("Log Orders")
 			.log("Order received: ${body}")
